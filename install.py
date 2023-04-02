58449114733a69ca44f6e0a7b814fc41095d7784
udon-universe <128375799+udon-universe@users.noreply.github.com>
2023-04-02 22:50:44 +0900

    implement: base

---
+++ install.py
@@ -0,0 +1,4 @@
+import launch
+
+# if not launch.is_installed("dummy"):
+#     launch.run_pip("install dummy==0.6.0", "requirements for XXX")
---
+++ scripts/bench.py
@@ -0,0 +1,23 @@
+import modules.scripts as scripts
+import gradio as gr
+import os
+
+from modules import script_callbacks
+
+def on_ui_tabs():
+    with gr.Blocks(analytics_enabled=False) as ui_component:
+        with gr.Row():
+            angle = gr.Slider(
+                minimum=0.0,
+                maximum=360.0,
+                step=1,
+                value=0,
+                label="Angle"
+            )
+            checkbox = gr.Checkbox(
+                False,
+                label="Checkbox"
+            )
+        return [(ui_component, "Benchmark", "benchmark_tab")]
+
+script_callbacks.on_ui_tabs(on_ui_tabs)
